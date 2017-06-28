fe67faca817053e7f1d635246f2836a935ae1be6
progman <gnuplanet@gmail.com>
2017-06-28 14:41:44 +0300

    update test

--- test.php
+++ test.php
@@ -20,63 +20,63 @@ function test__libcore__tojson()
 	$rc = libcore__tojson(" ");
 	if (strcmp($rc, ' ') !== 0)
 	{
-		echo "ERROR[test__libcore__tojson()]: step001\n";
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
 		exit(1);
 	}
 
 	$rc = libcore__tojson("\x22");
 	if (strcmp($rc, '\"') !== 0)
 	{
-		echo "ERROR[test__libcore__tojson()]: step002\n";
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
 		exit(1);
 	}
 
 	$rc = libcore__tojson("\x5c");
 	if (strcmp($rc, "\\\\") !== 0)
 	{
-		echo "ERROR[test__libcore__tojson()]: step003\n";
+		echo "ERROR[".__FUNCTION__."()]: step003\n";
 		exit(1);
 	}
 
 	$rc = libcore__tojson("\x2f");
 	if (strcmp($rc, '\/') !== 0)
 	{
-		echo "ERROR[test__libcore__tojson()]: step004\n";
+		echo "ERROR[".__FUNCTION__."()]: step004\n";
 		exit(1);
 	}
 
 	$rc = libcore__tojson("\x08");
 	if (strcmp($rc, '\b') !== 0)
 	{
-		echo "ERROR[test__libcore__tojson()]: step005\n";
+		echo "ERROR[".__FUNCTION__."()]: step005\n";
 		exit(1);
 	}
 
 	$rc = libcore__tojson("\x0c");
 	if (strcmp($rc, '\f') !== 0)
 	{
-		echo "ERROR[test__libcore__tojson()]: step006\n";
+		echo "ERROR[".__FUNCTION__."()]: step006\n";
 		exit(1);
 	}
 
 	$rc = libcore__tojson("\x0a");
 	if (strcmp($rc, '\n') !== 0)
 	{
-		echo "ERROR[test__libcore__tojson()]: step007\n";
+		echo "ERROR[".__FUNCTION__."()]: step007\n";
 		exit(1);
 	}
 
 	$rc = libcore__tojson("\x0d");
 	if (strcmp($rc, '\r') !== 0)
 	{
-		echo "ERROR[test__libcore__tojson()]: step008\n";
+		echo "ERROR[".__FUNCTION__."()]: step008\n";
 		exit(1);
 	}
 
 	$rc = libcore__tojson("\x09");
 	if (strcmp($rc, '\t') !== 0)
 	{
-		echo "ERROR[test__libcore__tojson()]: step009\n";
+		echo "ERROR[".__FUNCTION__."()]: step009\n";
 		exit(1);
 	}
 }
@@ -85,19 +85,19 @@ function test__libcore__is_uint()
 {
 	if (libcore__is_uint("") == true)
 	{
-		echo "ERROR[test_libcore__is_uint()]: step001\n";
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
 		exit(1);
 	}
 
 	if (libcore__is_uint("+1") == true)
 	{
-		echo "ERROR[test_libcore__is_uint()]: step002\n";
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
 		exit(1);
 	}
 
 	if (libcore__is_uint("-1") == true)
 	{
-		echo "ERROR[test_libcore__is_uint()]: step003\n";
+		echo "ERROR[".__FUNCTION__."()]: step003\n";
 		exit(1);
 	}
 }
@@ -106,20 +106,20 @@ function test__libcore__is_sint()
 {
 	if (libcore__is_sint("") == true)
 	{
-		echo "ERROR[test_libcore__is_sint()]: step001\n";
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
 		exit(1);
 	}
 
 	if (libcore__is_sint("+1+") == true)
 	{
-		echo "ERROR[test_libcore__is_sint()]: step002\n";
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
 		exit(1);
 	}
 
 
 	if (libcore__is_sint("-1-") == true)
 	{
-		echo "ERROR[test_libcore__is_sint()]: step003\n";
+		echo "ERROR[".__FUNCTION__."()]: step003\n";
 		exit(1);
 	}
 }
@@ -128,19 +128,19 @@ function test__libcore__is_hex()
 {
 	if (libcore__is_hex("") == true)
 	{
-		echo "ERROR[test_libcore__is_hex()]: step001\n";
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
 		exit(1);
 	}
 
 	if (libcore__is_hex("z") == true)
 	{
-		echo "ERROR[test_libcore__is_hex()]: step002\n";
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
 		exit(1);
 	}
 
 	if (libcore__is_hex("0") == false)
 	{
-		echo "ERROR[test_libcore__is_hex()]: step003\n";
+		echo "ERROR[".__FUNCTION__."()]: step003\n";
 		exit(1);
 	}
 
@@ -148,14 +148,14 @@ for ($i=0; $i < 1000000; $i++)
 {
 	if (libcore__is_hex("0123456789abcdefABCDEF") == false)
 	{
-		echo "ERROR[test_libcore__is_hex()]: step004\n";
+		echo "ERROR[".__FUNCTION__."()]: step004\n";
 		exit(1);
 	}
 }
 
 	if (libcore__is_hex("0123456789abcdefABCDEF0", true) == true)
 	{
-		echo "ERROR[test_libcore__is_hex()]: step005\n";
+		echo "ERROR[".__FUNCTION__."()]: step005\n";
 		exit(1);
 	}
 }
@@ -164,43 +164,43 @@ function test__libcore__is_float()
 {
 	if (libcore__is_float("") == true)
 	{
-		echo "ERROR[test_libcore__is_float()]: step001\n";
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
 		exit(1);
 	}
 
 	if (libcore__is_float("1") == false)
 	{
-		echo "ERROR[test_libcore__is_float()]: step002\n";
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
 		exit(1);
 	}
 
 	if (libcore__is_float("1.00") == false)
 	{
-		echo "ERROR[test_libcore__is_float()]: step002\n";
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
 		exit(1);
 	}
 
 	if (libcore__is_float("1.0.0") == true)
 	{
-		echo "ERROR[test_libcore__is_float()]: step002\n";
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
 		exit(1);
 	}
 
 	if (libcore__is_float("1.") == true)
 	{
-		echo "ERROR[test_libcore__is_float()]: step003\n";
+		echo "ERROR[".__FUNCTION__."()]: step003\n";
 		exit(1);
 	}
 
 	if (libcore__is_float(".1") == true)
 	{
-		echo "ERROR[test_libcore__is_float()]: step004\n";
+		echo "ERROR[".__FUNCTION__."()]: step004\n";
 		exit(1);
 	}
 
 	if (libcore__is_float(".1.") == true)
 	{
-		echo "ERROR[test_libcore__is_float()]: step005\n";
+		echo "ERROR[".__FUNCTION__."()]: step005\n";
 		exit(1);
 	}
 }
@@ -215,7 +215,7 @@ function test__libcore__hex2bin()
 
 	if ($x !== $y)
 	{
-		echo "ERROR[test__libcore__hex2bin()]: step001\n";
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
 		exit(1);
 	}
 }
@@ -224,13 +224,13 @@ function test__libcore__is_parity()
 {
 	if (libcore__is_parity(1) == true)
 	{
-		echo "ERROR[test__libcore__is_parity()]: step001\n";
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
 		exit(1);
 	}
 
 	if (libcore__is_parity(2) == false)
 	{
-		echo "ERROR[test__libcore__is_parity()]: step002\n";
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
 		exit(1);
 	}
 }
@@ -248,7 +248,7 @@ function test__libcore__rnd()
 
 			if ($x == ($max + 1))
 			{
-				echo "ERROR[test__libcore__rnd()]: step001\n";
+				echo "ERROR[".__FUNCTION__."()]: step001\n";
 				exit(1);
 			}
 
@@ -256,7 +256,7 @@ function test__libcore__rnd()
 
 			if ($count == 1000000000)
 			{
-				echo "ERROR[test__libcore__rnd()]: step002\n";
+				echo "ERROR[".__FUNCTION__."()]: step002\n";
 				exit(1);
 			}
 		}
@@ -268,14 +268,14 @@ function test__libcore__hex_string_parity()
 	$x = libcore__hex_string_parity("0");
 	if (strcmp($x, "00") != 0)
 	{
-		echo "ERROR[test__libcore__hex_string_parity()]: step001\n";
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
 		exit(1);
 	}
 
 	$x = libcore__hex_string_parity("00");
 	if (strcmp($x, "00") != 0)
 	{
-		echo "ERROR[test__libcore__hex_string_parity()]: step002\n";
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
 		exit(1);
 	}
 }
@@ -285,14 +285,14 @@ function test__libcore__hex_string_expand()
 	$x = libcore__hex_string_expand("0", 2);
 	if (strcmp($x, "00") != 0)
 	{
-		echo "ERROR[test__libcore__hex_string_expand()]: step001\n";
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
 		exit(1);
 	}
 
 	$x = libcore__hex_string_expand("00", 2);
 	if (strcmp($x, "00") != 0)
 	{
-		echo "ERROR[test__libcore__hex_string_expand()]: step002\n";
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
 		exit(1);
 	}
 }
@@ -304,7 +304,7 @@ function test__libcore__hex_string_add()
 	$z = libcore__hex_string_add($x, $y);
 	if (strcmp($z, "03") != 0)
 	{
-		echo "ERROR[test__libcore__hex_string_add()]: step001\n";
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
 		exit(1);
 	}
 
@@ -314,7 +314,7 @@ function test__libcore__hex_string_add()
 	$z = libcore__hex_string_add($x, $y);
 	if (strcmp($z, "ff") != 0)
 	{
-		echo "ERROR[test__libcore__hex_string_add()]: step002\n";
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
 		exit(1);
 	}
 
@@ -324,7 +324,7 @@ function test__libcore__hex_string_add()
 	$z = libcore__hex_string_add($x, $y);
 	if (strcmp($z, "0100") != 0)
 	{
-		echo "ERROR[test__libcore__hex_string_add()]: step003\n";
+		echo "ERROR[".__FUNCTION__."()]: step003\n";
 		exit(1);
 	}
 
@@ -333,7 +333,7 @@ function test__libcore__hex_string_add()
 	$z = libcore__hex_string_add($x, $y);
 	if (strcmp($z, "01fe") != 0)
 	{
-		echo "ERROR[test__libcore__hex_string_add()]: step004\n";
+		echo "ERROR[".__FUNCTION__."()]: step004\n";
 		exit(1);
 	}
 
@@ -350,7 +350,7 @@ function test__libcore__hex_string_add()
 
 		if (strcmp($c1, $c2) != 0)
 		{
-			echo "ERROR[test__libcore__hex_string_expand()]: step005\n";
+			echo "ERROR[".__FUNCTION__."()]: step005\n";
 			echo "c1     : ".$c1."\n";
 			echo "c2     : ".$c2."\n";
 			exit(1);
