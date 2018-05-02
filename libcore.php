5550ece0507a44c2d42370f58b30e765a925e3aa
progman <gnuplanet@gmail.com>
2018-05-02 20:05:48 +0300

    add libcore__is_hex_char()

--- libcore.php
+++ libcore.php
@@ -1,6 +1,6 @@
 <?php
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-// 0.5.6
+// 0.5.7
 // Alexey Potehin <gnuplanet@gmail.com>, http://www.gnuplanet.ru/doc/cv
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // result object
@@ -121,27 +121,6 @@ class result_t
 		return $this->flag_skip;
 	}
 }
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-static $libcore__hex2bin_table = array
-(
-//	0x00  0x01  0x02  0x03  0x04  0x05  0x06  0x07  0x08  0x09  0x0A  0x0B  0x0C  0x0D  0x0E  0x0F
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x00
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x10
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x20
-	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, null, null, null, null, null, null, // 0x30
-	null, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, null, null, null, null, null, null, null, null, null, // 0x40
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x50
-	null, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, null, null, null, null, null, null, null, null, null, // 0x60
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x70
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x80
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x90
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0xA0
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0xB0
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0xC0
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0xD0
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0xE0
-	null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null  // 0xF0
-);
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 /**
  * convert string for json string
@@ -399,9 +378,6 @@ function is_uuid($uuid, $uuid_type = 'ANY')
 '{a0eebc999c0b4ef8bb6d6bb9bd380a17}'     // 34
 'a0eebc999c0b4ef8bb6d6bb9bd380a16'       // 32
 */
-	global $libcore__hex2bin_table;
-
-
 	if (strcmp(gettype($uuid), 'string') !== 0)
 	{
 		return false;
@@ -450,7 +426,7 @@ function is_uuid($uuid, $uuid_type = 'ANY')
 	{
 		if ($template[$i] === 'x')
 		{
-			if ($libcore__hex2bin_table[ord($uuid[$i])] === null) return false;
+			if (libcore__is_hex_char(ord($uuid[$i])) === false) return false;
 		}
 		else
 		{
@@ -536,6 +512,57 @@ function libcore__is_sint($x)
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 /**
+ * check hex char
+ * \param[in] val source value
+ * \return hex char flag
+ */
+function libcore__is_hex_char($val)
+{
+	static $libcore__hex2bin_table = array
+	(
+//	0x00  0x01  0x02  0x03  0x04  0x05  0x06  0x07  0x08  0x09  0x0A  0x0B  0x0C  0x0D  0x0E  0x0F
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x00
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x10
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x20
+		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, null, null, null, null, null, null, // 0x30
+		null, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, null, null, null, null, null, null, null, null, null, // 0x40
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x50
+		null, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, null, null, null, null, null, null, null, null, null, // 0x60
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x70
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x80
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0x90
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0xA0
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0xB0
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0xC0
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0xD0
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, // 0xE0
+		null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null  // 0xF0
+	);
+
+
+	if (strcmp(gettype($val), 'integer') !== 0)
+	{
+		return false;
+	}
+
+
+	if
+	(
+		($val < 0) ||
+		($val > 255)
+	)
+	{
+		return false;
+	}
+
+
+	if ($libcore__hex2bin_table[$val] !== null) return true;
+
+
+	return false;
+}
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+/**
  * check hex string
  * \param[in] val source value
  * \param[in] flag_parity parity flag
@@ -543,8 +570,6 @@ function libcore__is_sint($x)
  */
 function libcore__is_hex($x, $flag_parity = false)
 {
-	global $libcore__hex2bin_table;
-
 	settype($x, "string");
 
 	$size = strlen($x);
@@ -561,7 +586,7 @@ function libcore__is_hex($x, $flag_parity = false)
 
 	for ($i=0; $i < $size; $i++)
 	{
-		if ($libcore__hex2bin_table[ord($x[$i])] === null)
+		if (libcore__is_hex_char(ord($x[$i])) === false)
 		{
 			return false;
 		}
