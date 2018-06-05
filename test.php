832b0a8b609cfd97cb345264e0af772de277c010
progman <gnuplanet@gmail.com>
2018-06-05 17:49:08 +0300

    add libcore__uuid2type34(), libcore__uuid2type36()

--- test.php
+++ test.php
@@ -196,6 +196,14 @@ function test__libcore__is_parity()
 	}
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+/*
+ 0         1         2         3
+ 01234567890123456789012345678901234567
+'{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13}' // 38
+'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12'   // 36
+'{a0eebc999c0b4ef8bb6d6bb9bd380a17}'     // 34
+'a0eebc999c0b4ef8bb6d6bb9bd380a16'       // 32
+*/
 function test__libcore__is_uuid()
 {
 	if (libcore__is_uuid('{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12}') === false)
@@ -346,6 +354,14 @@ function test__libcore__is_uuid()
 	}
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+/*
+ 0         1         2         3
+ 01234567890123456789012345678901234567
+'{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13}' // 38
+'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12'   // 36
+'{a0eebc999c0b4ef8bb6d6bb9bd380a17}'     // 34
+'a0eebc999c0b4ef8bb6d6bb9bd380a16'       // 32
+*/
 function test__libcore__uuid2type32()
 {
 	$rc = libcore__uuid2type32('{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13}');
@@ -397,6 +413,132 @@ function test__libcore__uuid2type32()
 	}
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+ 0         1         2         3
+ 01234567890123456789012345678901234567
+'{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13}' // 38
+'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12'   // 36
+'{a0eebc999c0b4ef8bb6d6bb9bd380a17}'     // 34
+'a0eebc999c0b4ef8bb6d6bb9bd380a16'       // 32
+*/
+function test__libcore__uuid2type34()
+{
+	$rc = libcore__uuid2type34('{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13}');
+	if ($rc === false)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
+		exit(1);
+	}
+	if (strcmp($rc, '{a0eebc999c0b4ef8bb6d6bb9bd380a13}') !== 0)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
+		exit(1);
+	}
+
+	$rc = libcore__uuid2type34('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12');
+	if ($rc === false)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step003\n";
+		exit(1);
+	}
+	if (strcmp($rc, '{a0eebc999c0b4ef8bb6d6bb9bd380a12}') !== 0)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step004\n";
+		exit(1);
+	}
+
+	$rc = libcore__uuid2type34('{a0eebc999c0b4ef8bb6d6bb9bd380a17}');
+	if ($rc === false)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step005\n";
+		exit(1);
+	}
+	if (strcmp($rc, '{a0eebc999c0b4ef8bb6d6bb9bd380a17}') !== 0)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step006\n";
+		exit(1);
+	}
+
+	$rc = libcore__uuid2type34('a0eebc999c0b4ef8bb6d6bb9bd380a16');
+	if ($rc === false)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step007\n";
+		exit(1);
+	}
+	if (strcmp($rc, '{a0eebc999c0b4ef8bb6d6bb9bd380a16}') !== 0)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step008\n";
+		exit(1);
+	}
+}
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+ 0         1         2         3
+ 01234567890123456789012345678901234567
+'{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13}' // 38
+'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12'   // 36
+'{a0eebc999c0b4ef8bb6d6bb9bd380a17}'     // 34
+'a0eebc999c0b4ef8bb6d6bb9bd380a16'       // 32
+*/
+function test__libcore__uuid2type36()
+{
+	$rc = libcore__uuid2type36('{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13}');
+	if ($rc === false)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step001\n";
+		exit(1);
+	}
+	if (strcmp($rc, 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13') !== 0)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step002\n";
+		exit(1);
+	}
+
+	$rc = libcore__uuid2type36('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12');
+	if ($rc === false)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step003\n";
+		exit(1);
+	}
+	if (strcmp($rc, 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12') !== 0)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step004\n";
+		exit(1);
+	}
+
+	$rc = libcore__uuid2type36('{a0eebc999c0b4ef8bb6d6bb9bd380a17}');
+	if ($rc === false)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step005\n";
+		exit(1);
+	}
+	if (strcmp($rc, 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a17') !== 0)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step006\n";
+		exit(1);
+	}
+
+	$rc = libcore__uuid2type36('a0eebc999c0b4ef8bb6d6bb9bd380a16');
+	if ($rc === false)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step007\n";
+		exit(1);
+	}
+	if (strcmp($rc, 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a16') !== 0)
+	{
+		echo "ERROR[".__FUNCTION__."()]: step008\n";
+		exit(1);
+	}
+}
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+ 0         1         2         3
+ 01234567890123456789012345678901234567
+'{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13}' // 38
+'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12'   // 36
+'{a0eebc999c0b4ef8bb6d6bb9bd380a17}'     // 34
+'a0eebc999c0b4ef8bb6d6bb9bd380a16'       // 32
+*/
 function test__libcore__uuid2type38()
 {
 	$rc = libcore__uuid2type38('{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13}');
@@ -1197,6 +1339,8 @@ test__libcore__is_parity();
 test__libcore__is_uuid();
 
 test__libcore__uuid2type32();
+test__libcore__uuid2type34();
+test__libcore__uuid2type36();
 test__libcore__uuid2type38();
 
 test__libcore__is_uint();
