b11bde239d51f525073a7974ff43d07adb04e4e5
progman <gnuplanet@gmail.com>
2018-10-01 16:18:18 +0300

    add libcore__libcore__xml_to_object()

---
+++ src/function/other/!whitout_test/libcore__xml_to_object/libcore__xml_to_object.php
@@ -0,0 +1,42 @@
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+/**
+ * convert from xml to php object
+ * \param[in] xml
+ * \return php object
+ */
+function libcore__libcore__xml_to_object($xml)
+{
+	$result = new result_t(__FUNCTION__, __FILE__);
+
+
+	$tmp = '';
+	try
+	{
+		$tmp = new \SimpleXMLElement($xml);
+	}
+	catch(Exception $e)
+	{
+		$result->set_err(1, 'xml is broken');
+		return $result;
+	}
+
+	$tmp_json = json_encode($tmp);
+	if (json_last_error() !== JSON_ERROR_NONE)
+	{
+		$result->set_err(1, 'can not json encode');
+		return $result;
+	}
+
+	$tmp = json_decode($tmp_json);
+	if (json_last_error() !== JSON_ERROR_NONE)
+	{
+		$result->set_err(1, 'can not json decode');
+		return $result;
+	}
+
+
+	$result->set_ok();
+	$result->set_value($tmp);
+	return $result;
+}
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
